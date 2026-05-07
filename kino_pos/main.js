37730a033fdcedeeae2a39c442dce37496fe26f4
Carlo-Vqz <1722110547@utectulancingo.edu.mx>
2026-05-07 10:53:02 -0600

    feat: Add application icon for the main window and package configuration

--- kino_pos/main.js
+++ kino_pos/main.js
@@ -558,6 +558,7 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'build/pwa-512x512.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
